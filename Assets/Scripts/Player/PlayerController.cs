42171bb919c7b1ca4b27d95cbc3a94901f15212a
Plohoyprogrammist <plohoyprogrammist@gmail.com>
2025-05-18 17:55:19 +0700

    Сделал начальный код для передвижения

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float speed;
+    private Vector2 movement;
+    private Rigidbody2D rb2DPlayer;
+
+
+    private void Start()
     {
-        
+        rb2DPlayer = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        rb2DPlayer.AddForce(movement * speed * Time.fixedDeltaTime, ForceMode2D.Force);
     }
 }
