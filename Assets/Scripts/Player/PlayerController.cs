0c5690d86d0ffd79ab05c428dba21a0e65839c57
Plohoyprogrammist <plohoyprogrammist@gmail.com>
2025-05-19 11:05:49 +0700

    Ахуеть я сделал передвижение! Ашалеть!!!!

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public float blinckForce;
     private Vector2 movement;
     private Rigidbody2D rb2DPlayer;
+    private bool isBlinck = false;
 
 
     private void Start()
@@ -14,11 +16,25 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            isBlinck = true;
+        }
+        Debug.Log(movement + " " + isBlinck);
     }
 
     private void FixedUpdate()
     {
-        rb2DPlayer.MovePosition(rb2DPlayer.position + movement * speed * Time.fixedDeltaTime);
+        if (isBlinck && movement != Vector2.zero)
+        {
+            rb2DPlayer.AddForce(movement * blinckForce * Time.fixedDeltaTime, ForceMode2D.Impulse);
+            isBlinck = false;
+        }
+        else 
+        {
+            rb2DPlayer.AddForce(movement * speed * Time.fixedDeltaTime, ForceMode2D.Force);
+        }
+
     }
 }
